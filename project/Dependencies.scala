d230ae169b1712a35e43b166d111f4ac54cd334e
Matt Hicks <matt@courio.com>
2023-01-19 03:01:42 +0000

    Update fabric-io to 1.8.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object version {
     val profig: String = "3.4.7"
     val scribe: String = "3.10.6"
-    val fabric: String = "1.8.12"
+    val fabric: String = "1.8.13"
     val reactify: String = "4.0.8"
     val cats: String = "3.4.4"
     val fs2: String = "3.4.0"
