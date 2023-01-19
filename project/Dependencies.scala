e6b287b155427fe6266af9e7846bf9d23ae2a472
Matt Hicks <matt@courio.com>
2023-01-19 03:01:26 +0000

    Update fs2-core to 3.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val fabric: String = "1.8.12"
     val reactify: String = "4.0.8"
     val cats: String = "3.4.4"
-    val fs2: String = "3.4.0"
+    val fs2: String = "3.5.0"
     val literally: String = "1.1.0"
     val okHttp: String = "4.10.0"
     val undertow: String = "2.2.22.Final"
