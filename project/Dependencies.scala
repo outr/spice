fee34bd67336f92f3dcc239564a0cbf53e9c40bc
Matt Hicks <matt@courio.com>
2024-06-03 00:43:35 +0000

    Update scribe, scribe-cats, scribe-slf4j to 3.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object version {
     val profig: String = "3.4.14"
     
-    val scribe: String = "3.13.5"
+    val scribe: String = "3.14.0"
 
     val fabric: String = "1.14.5"
     
