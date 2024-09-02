15a02c42dc89d6c19b90aa70267a33e65e160679
Matt Hicks <matt@matthicks.com>
2024-09-02 00:08:21 +0000

    Update undertow-core to 2.3.17.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
 
     val httpMime: String = "4.5.14"
     
-    val undertow: String = "2.3.16.Final"
+    val undertow: String = "2.3.17.Final"
     
     val moduload: String = "1.1.7"
 
