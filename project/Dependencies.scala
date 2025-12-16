b057d74444288213ab8865935880274e520bcc9c
Matt Hicks <matt@matthicks.com>
2025-12-16 00:11:16 +0000

    Update netty-buffer, netty-codec-http, ... to 4.2.9.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
     
     val okHttp: String = "5.3.0"
 
-    val netty: String = "4.2.7.Final"
+    val netty: String = "4.2.9.Final"
 
     val httpMime: String = "4.5.14"
     
